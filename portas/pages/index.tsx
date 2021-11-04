b563c156e9d6a3598131938299859a20357ea926
kleitonalbuquerque <kleiton2102@gmail.com>
2021-11-04 15:05:21 -0300

    Add eventos na porta

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import Porta from "../components/Porta";
-import Presente from "../components/Presente";
 import PortaModel from "../model/porta";
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1));
+  const [texto, setTexto] = useState("...");
 
   return (
-    <div style={{ display: "flex" }}>
-      <Porta porta={p1} />
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Porta value={p1} onChange={(novaPorta) => setP1(novaPorta)} />
     </div>
   );
 }
